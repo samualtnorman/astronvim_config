2d0dc5d4cb6ae8892ee42f65e60b39e3ab45bbe8
Samual Norman <me@samual.uk>
2023-07-31 10:17:32 +0100

    indent, disable autoformat, typescript, lua, add some plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,60 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    {
+        "ray-x/lsp_signature.nvim",
+        event = "BufRead",
+        config = function()
+            require("lsp_signature").setup()
+        end,
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+    {
+        "smoka7/multicursors.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'smoka7/hydra.nvim'
+        },
+        opts = function()
+            local N = require 'multicursors.normal_mode'
+            local I = require 'multicursors.insert_mode'
+
+            return {
+                normal_keys = {
+                    -- to change default lhs of key mapping change the key
+                    [','] = {
+                        -- assigning nil to method exits from multi cursor mode
+                        method = N.clear_others,
+                        -- you can pass :map-arguments here
+                        opts = { desc = 'Clear others' }
+                    }
+                },
+                insert_keys = {
+                    -- to change default lhs of key mapping change the key
+                    ['<CR>'] = {
+                        -- assigning nil to method exits from multi cursor mode
+                        method = I.Cr_method,
+                        -- you can pass :map-arguments here
+                        opts = { desc = 'New line' }
+                    }
+                }
+            }
+        end,
+        cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+        keys = {
+            {
+                mode = { 'v', 'n' },
+                '<Leader>m',
+                '<cmd>MCstart<cr>',
+                desc = 'Create a selection for selcted text or word under the cursor',
+            }
+        }
+    }
 }
